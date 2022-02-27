f5f44aa49345c68dec7101fd984c08b85b2b016d
HeyManBeepo <69164211+HeyManBeepo@users.noreply.github.com>
2022-02-27 11:03:17 -0800

    HEEHEEHAW
    Worked on quiz

--- src/submitButton.js
+++ src/submitButton.js
@@ -1,42 +1,51 @@
-import React from 'react';
-class SubmitButton extends React.Component {
+import React, {useState} from 'react';
+import dogVideo from './golden.gif';
+class SubmitButton extends React.Component{
     // This syntax ensures `this` is bound within handleClick.
     // Warning: this is *experimental* syntax.
-    constructor(props) {
-      super(props);
-      this.state = {
-          value: 'fartpoop',
-          isCorrectAnswer: true,
-      }
-    };
-    correctAnswer = '$30';
-    onChange = (event) => {
-      this.setState({ value: event.target.value});
+  constructor(props){
+    super(props);
+    this.state = {
+      answer : '$30',
+      submitted : 'false',
+      answer2 : 'treato',
+    }
   }
-    onSubmit = (event) => {
-      const { value } = this.state;
-      event.preventDefault();
-      console.log(this.state.value);
-      console.log(value);
-      (this.state.value == this.correctAnswer ? this.setState({isCorrectAnswer: true}) : this.setState({isCorrectAnswer: false})) 
-  }
-    
-  render() {
-    return (
-      <form>
+    onChange = (e) => {
+      this.setState({answer: e.target.value});
+    }
+    onChange2 = (e) => {
+      this.setState({answer2: e.target.value});
+    }
+    onSubmit = (e) => {
+      this.setState({submitted : true});
+      e.preventDefault();
+    }
+    render(){
+      return (
+        <form>
           <label>
             Email the question??!? Need to type in $30 first plz gimme some treats.
             <input type = "text" name = "name" onChange = {(event) => {this.onChange(event)}} />
           </label>
-          <button onClick={(event) => {this.onSubmit(event)}}>
+      {this.state.answer == "$30" ? <div><h1> Corect!</h1><img src = {dogVideo} /></div> : <h2>False!</h2>}
+      <button onClick={(event) => this.onSubmit()}>
         Submit
       </button>
-      {this.state.isCorrectAnswer ? <h1> Corect!</h1> : <h2>False!</h2>}
-      
-      
-        </form>
+      <label> 
+        Thamks for the 30$. Gimme a treato now.
+        <input type = "text" name = "name" onChange = {(event) => {this.onChange(event)}} />
+        </label>
+        <button onClick={(event) => this.onSubmit()}>
+        Submit
+      </button>
+        Treato.
+        {this.state.answer2 == "treato"? <div><h1> Yum ha</h1><img src = {dogVideo} /></div> : <h2>False!</h2>}
+      <label> 
+        Hee hee hee haw.
+      </label>
+      </form> 
     );
-  }
-}
+  };
+};
 export default SubmitButton;
- 
